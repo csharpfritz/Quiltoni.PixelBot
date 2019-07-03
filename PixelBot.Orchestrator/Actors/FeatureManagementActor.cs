44a296f4c7863ca17fd73d69b6ce747e3be97f1d
Jeff Fritz <csharpfritz@users.noreply.github.com>
2019-07-03 08:15:37 -0400

    WIP

--- PixelBot.Orchestrator/Actors/FeatureManagementActor.cs
+++ PixelBot.Orchestrator/Actors/FeatureManagementActor.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace PixelBot.Orchestrator.Actors
 {
-	public class FeatureManagementActor
+	public class FeatureManagementActor : ReceiveActor
 	{
+
+		public FeatureManagementActor(IEnumerable<Type> featureTypes) {
+
+			this.FeatureTypes = featureTypes;
+
+		}
+
+		public IEnumerable<Type> FeatureTypes { get; }
 
 		// TODO: Manage the collection of registered features
 		// TODO: Allow features to be requested of an StreamEvent Type
+
+		public static IActorRef Create(IEnumerable<Type> featureTypes) {
+
+			var p = Props.Create<FeatureManagementActor>(featureTypes);
+			return Context.ActorOf(p, "featuremanagement");
+
+		}
+
 
 	}
 }
