5203aa1221eef88a0dc916b5e5f4377efb6e323e
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-09-15 11:00:17 -0400

    Fixed the PixelBot with new HttpClient extension methods

---
+++ Quiltoni.PixelBot/HttpClientExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Quiltoni.PixelBot
+{
+	public static class HttpClientExtensions
+	{
+		public static Task<HttpResponseMessage> PostAsJsonAsync<T>(
+				this HttpClient httpClient, string url, T data)
+		{
+			var dataAsString = JsonConvert.SerializeObject(data);
+			var content = new StringContent(dataAsString);
+			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			return httpClient.PostAsync(url, content);
+		}
+
+		public static Task<HttpResponseMessage> PutAsJsonAsync<T>(
+				this HttpClient httpClient, string url, T data)
+		{
+			var dataAsString = JsonConvert.SerializeObject(data);
+			var content = new StringContent(dataAsString);
+			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			return httpClient.PutAsync(url, content);
+		}
+
+		public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+		{
+			var dataAsString = await content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<T>(dataAsString);
+		}
+	}
+}
