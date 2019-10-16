6aa4b7b7b6a05868ce280f768e81524a16318a4a
Wolfgang Stelzhammer <969932+Stelzi79@users.noreply.github.com>
2019-10-16 21:55:32 +0200

    added implementations to project files

--- PixelBot.Orchestrator/Startup.cs
+++ PixelBot.Orchestrator/Startup.cs
@@ -22,6 +22,8 @@ using PixelBot.Orchestrator.Components;
 using PixelBot.Orchestrator.Data;
 using PixelBot.Orchestrator.Services;
 using PixelBot.Orchestrator.Services.Authentication;
+using PixelBot.ResolverActors;
+using PixelBot.ResolverActors.Actors;
 using PixelBot.StandardFeatures.ScreenWidgets.ChatRoom;
 using Quiltoni.PixelBot.Core.Client;
 using Quiltoni.PixelBot.Core.Data;
@@ -35,22 +37,71 @@ namespace PixelBot.Orchestrator
 
 		public static BotConfiguration BotConfiguration { get; private set; }
 
-		public Startup(IConfiguration config) {
+		public Startup(IConfiguration config)
+		{
 
 			Configuration = config;
 		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services) {
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
 
-			services.ConfigureAspNet();
+			IdentityModelEventSource.ShowPII = true;
 
+
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			})
+			.AddCookie()
+			.AddAuth0OpenIdConnect(Configuration);
+
+			services.AddAuthorization(config =>
+			{
+
+				config.AddPolicy(nameof(Policy.GlobalAdmin), p =>
+				{
+					p.RequireRole("GlobalAdmin");
+				});
+			});
+
+
+			services.AddMvc()
+					.AddNewtonsoftJson();
+
+			services.AddHttpContextAccessor();
+			services.AddScoped<ClaimsPrincipal>(context => context.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.User);
+
+			services.AddControllers();
+
+			services.AddServerSideBlazor();
+			services.AddSignalR(config =>
+			{
+				config.EnableDetailedErrors = true;
+			}).AddJsonProtocol();
 
 			services.Configure<BotConfiguration>(Configuration.GetSection("BotConfig"));
 
 			services.ConfigureApplicationServices();
 
-			services.AddHttpClient("TwitchHelixApi", config => {
+			// Adds a ResolveServiceActor to the DI system
+			services.AddResolveActor<ResolveServicesActor>();
+
+			services.AddTransient<IChannelConfigurationContext, FileStorageChannelConfigurationContext>();
+			services.AddTransient<IFollowerDedupeService, InMemoryFollowerDedupeService>();
+
+			services.AddHttpClient("TwitchHelixApi", config =>
+			{
 
 				config.BaseAddress = new Uri(Configuration["TwitchAPI:EndpointURL"]);
 				config.DefaultRequestHeaders.Add("Accept", @"application/json");
@@ -58,14 +109,15 @@ namespace PixelBot.Orchestrator
 
 			});
 
-			// Cheer 100 ramblinggeek 19/4/19 
- 
-			services.ConfigureActorModel(); 
+			// Cheer 100 ramblinggeek 19/4/19
+
+			services.ConfigureActorModel();
 
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<BotConfiguration> botConfig, ILoggerFactory loggerFactory) {
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<BotConfiguration> botConfig, ILoggerFactory loggerFactory)
+		{
 
 			// cheer 142 cpayette 4/10/2019
 			// cheer 300 tbdgamer 4/10/2019
@@ -75,10 +127,12 @@ namespace PixelBot.Orchestrator
 
 			BotConfiguration = botConfig.Value;
 
-			if (env.IsDevelopment()) {
+			if (env.IsDevelopment())
+			{
 				app.UseDeveloperExceptionPage();
 			}
-			else {
+			else
+			{
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
@@ -92,11 +146,15 @@ namespace PixelBot.Orchestrator
 			app.UseAuthentication();
 			app.UseAuthorization();
 
+			// Initializes all ResolveActors
+			app.UseResolveActors();
+
 			// Configure plugged in features
 			PluginBootstrapper.ServiceProvider = app.ApplicationServices;
 			PluginBootstrapper.InitializeFeatures(app);
 
-			app.UseEndpoints(routes => {
+			app.UseEndpoints(routes =>
+			{
 
 				routes.MapHub<LoggerHub>("/loggerhub");
 				routes.MapHub<UserActivityHub>("/useractivityhub");
@@ -109,8 +167,9 @@ namespace PixelBot.Orchestrator
 			});
 		}
 
-		private static void MapExternalHubs(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder routes) {
-			
+		private static void MapExternalHubs(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder routes)
+		{
+
 			// TODO: use reflection to identify Hubs in the StandardFeatures assembly and add them
 			routes.MapHub<ChatRoomHub>("/hubs/chatroom");
 
