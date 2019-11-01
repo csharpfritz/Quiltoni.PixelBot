5beb932ded9a266c67e6c53214a08c1edb98a7fe
Wolfgang Stelzhammer <969932+Stelzi79@users.noreply.github.com>
2019-11-01 03:38:43 +0100

    added some missed formatting

--- PixelBot.ReplayLog/LogRecord.cs
+++ PixelBot.ReplayLog/LogRecord.cs
@@ -9,23 +9,27 @@ namespace PixelBot.ReplayLog
 	public class LogRecord
 	{
 
-		[CsvColumn(Name="Date", FieldIndex=1)]
+		[CsvColumn(Name = "Date", FieldIndex = 1)]
 		public string Date { get; set; }
 
-		public DateTime DateStamp { get {
+		public DateTime DateStamp
+		{
+			get
+			{
 				return DateTime.ParseExact(Date, "MMM dd, yyyy - h:mm:ss tt", CultureInfo.InvariantCulture);
-			} }
+			}
+		}
 
-		[CsvColumn(Name ="Acting User", FieldIndex =2)]
+		[CsvColumn(Name = "Acting User", FieldIndex = 2)]
 		public string ActingUser { get; set; }
 
-		[CsvColumn(Name ="Record Updated", FieldIndex =3)]
+		[CsvColumn(Name = "Record Updated", FieldIndex = 3)]
 		public string UpdatedUser { get; set; }
 
-		[CsvColumn(Name ="Command", FieldIndex =4)]
+		[CsvColumn(Name = "Command", FieldIndex = 4)]
 		public string Command { get; set; }
 
-		[CsvColumn(Name ="Pixels Changed", FieldIndex =5)]
+		[CsvColumn(Name = "Pixels Changed", FieldIndex = 5)]
 		public int Changed { get; set; }
 
 	}
